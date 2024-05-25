9e8f0cbb549120380b9519da2b9952558cb88cea
root <root@fourthpyramid.localdomain>
2024-05-25 09:06:53 +0300

    new update

--- 01-Fundamentals-Part-1/our_practice_code/script.js
+++ 01-Fundamentals-Part-1/our_practice_code/script.js
@@ -2,6 +2,7 @@ let js = "amazing";
 // if (js === "amazing") alert("javascript is amazing");
 alert("we finished in javascript course the first section till 009 lecture");
 alert("and we set 1 hour and finished 1 hour from the video course with practice");
+alert("and all is commited and pushed on github repo");
 console.log(40 + 8 + 23 - 10);
 console.log(40 + 8 + 23 - 10);
 console.log(40 + 8 + 23 - 10);

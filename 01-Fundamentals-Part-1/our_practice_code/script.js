18622ed63693bb0b9d3a4b30719d7d75fc9541d9
root <root@fourthpyramid.localdomain>
2024-05-25 09:05:49 +0300

    new update

---
+++ 01-Fundamentals-Part-1/our_practice_code/script.js
@@ -0,0 +1,8 @@
+let js = "amazing";
+// if (js === "amazing") alert("javascript is amazing");
+alert("we finished in javascript course the first section till 009 lecture");
+alert("and we set 1 hour and finished 1 hour from the video course with practice");
+console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
+console.log("we finished till section 2 and till 009")
